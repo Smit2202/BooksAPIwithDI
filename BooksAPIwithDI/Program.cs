d51fc59f3551f5372e397c2d3cac5cc5ce46126d
Smit2202 <smit.bhanushali@arishti.com>
2024-03-19 17:35:38 +0530

    Last Update

--- BooksAPIwithDI/Program.cs
+++ BooksAPIwithDI/Program.cs
@@ -1,3 +1,5 @@
+using SampleAspNetCore.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,15 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+//builder.Services.AddSingleton<IBookRepository, BookRepository>();
+
+//builder.Services.AddScoped<IBookRepository, BookRepository>();
+
+builder.Services.AddTransient<IBookRepository, BookRepository>();
+
+// Register other dependencies here
 
 var app = builder.Build();
 
---
+++ BooksAPIwithDI/Repositories/BookRepository.cs
@@ -0,0 +1,21 @@
+﻿using SampleAspNetCore.Models;
+using System.Collections.Generic;
+
+namespace SampleAspNetCore.Repositories
+{
+    public class BookRepository : IBookRepository
+    {
+        private List<BookModel> books = new List<BookModel>();
+        public int AddBook(BookModel book)
+        {
+            book.Id = books.Count + 1; 
+            books.Add(book);
+            return book.Id;
+        }
+        public List<BookModel> GetAllBooks()
+        {
+            return books;
+        }
+
+    }
+}
